a2028a09c61dbe6365eaabde7a6b1898f3d32ba0
tcodeabbot <tubeineabbot01@gmail.com>
2025-09-23 20:34:20 -0400

    FIxed an issue where the videoPlayer was not being embeded into the learning page

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,7 +1,13 @@
 "use client";
 
-import ReactPlayer from "react-player";
-// const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
+import dynamic from "next/dynamic";
+
+const ReactPlayer = dynamic(() => import("react-player"), {
+  ssr: false,
+  loading: () => (
+    <div className="relative aspect-video bg-gray-200 animate-pulse" />
+  )
+}) as any;
 
 interface VideoPlayerProps {
   url: string;
@@ -9,10 +15,9 @@ interface VideoPlayerProps {
 
 export const VideoPlayer = ({ url }: VideoPlayerProps) => {
   return (
-    //
-    <div>
+    <div className="relative aspect-video">
       <ReactPlayer
-        url={url}
+        src={url}
         width="100%"
         height="100%"
         controls
